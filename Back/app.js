855d6469a7ea5c71506c6ca5d7826e5d3fd5198d
Peter94157 <pedroleonardo2001@gmail.com>
2025-06-07 17:34:08 -0300

    FIX

--- Back/app.js
+++ Back/app.js
@@ -5,48 +5,8 @@ const app = express();
 const port = process.env.PORT || 5500
 const Category = require('./Models/Category')
 app.use(express.json());
-
-
-
-const categoryQuestions = {
-    Animais: [
-        { description: "Animal que mia", answer: "Gato" },
-        { description: "Maior animal terrestre", answer: "Elefante" },
-        { description: "Mamífero que voa", answer: "Morcego" },
-        { description: "Rei da selva", answer: "Leão" },
-        { description: "Animal que põe ovos e tem bico", answer: "Pato" },
-    ],
-    História: [
-        { description: "Ano da independência do Brasil", answer: "1822" },
-        { description: "Primeira Guerra Mundial começou em qual ano?", answer: "1914" },
-        { description: "Quem descobriu o Brasil?", answer: "Pedro Álvares Cabral" },
-        { description: "Nome da cidade onde Napoleão nasceu", answer: "Ajaccio" },
-        { description: "Qual foi o primeiro imperador do Brasil?", answer: "Dom Pedro I" },
-    ],
-    Ciência: [
-        { description: "Elemento químico essencial para respirar", answer: "Oxigênio" },
-        { description: "O planeta vermelho", answer: "Marte" },
-        { description: "Unidade básica da vida", answer: "Célula" },
-        { description: "Nome do cientista que formulou a teoria da relatividade", answer: "Einstein" },
-        { description: "Fenômeno que permite um avião voar", answer: "Aerodinâmica" },
-    ],
-    Esportes: [
-        { description: "Esporte mais popular do Brasil", answer: "Futebol" },
-        { description: "País que sediou as Olimpíadas de 2016", answer: "Brasil" },
-        { description: "Esporte jogado com raquete e rede", answer: "Tênis" },
-        { description: "Quantos jogadores tem um time de vôlei?", answer: "6" },
-        { description: "Esporte em que se luta dentro de um octógono", answer: "MMA" },
-    ],
-    Cinema: [
-        { description: "Filme onde um robô se apaixona pela robô Eva", answer: "Wall-E" },
-        { description: "Filme famoso sobre dinossauros", answer: "Jurassic Park" },
-        { description: "Série de filmes com sabres de luz", answer: "Star Wars" },
-        { description: "Super-herói milionário da Marvel", answer: "Homem de Ferro" },
-        { description: "Filme onde um peixe chamado Nemo é perdido", answer: "Procurando Nemo" },
-    ],
-};
-
-
+const cors = require('cors');
+app.use(cors());
 
 
 
